1c6c87d58bae97905e15c35c448394bfdf32421d
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-08-16 04:51:49 -0400

    App imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Header from "./Header.js"
+import DigimonContainer from './DigimonContainer.js'
+import TeamContainer from './TeamContainer.js'
+import Filter from "./Filter.js"
+import RateMyTeam from "./RateMyTeam.js"
+import Form from "./Form.js"
+import NewDigimonContainer from "./NewDigimonContainer.js"
+import NavBar from "./NavBar.js"
 import './App.css';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>
-
-      </h1>
+      
     </div>
   );
 }
