419d5c94469b1d54aef1570ea93eb0a13ae361e7
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-08-16 05:17:08 -0400

    Digimon

---
+++ src/components/Digimon.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Digimon( { digimon, team, setTeam } ){
+
+    function handleClick(){
+
+        const newDigimon = {
+            "name" : digimon.name,
+            "img" : digimon.img,
+            "level" : digimon.level
+        }
+
+        if(team.length >= 6){
+            return alert("Limit Reached!")
+        } else {
+            fetch(`http://localhost:8000/Digimon`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newDigimon),
+        })
+        .then((r) => r.json())
+        .then((newDigimon) => setTeam([...team, newDigimon]))
+        }
+    }
+
+    return (
+        <div className="test" onClick={handleClick}>
+            <p className='name'>{digimon.name}</p>
+            <img width="165px" src={digimon.img} alt={digimon.name}/>
+        </div>
+    );
+
+}
+
+export default Digimon;
